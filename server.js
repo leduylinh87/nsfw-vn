446a10d0631d57f553b7793b91eccf98f7050080
leduylinh87 <leduylinh87@gmail.com>
2015-09-23 00:25:55 +0700

    Update server.js
    Create object for server

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var http = require('http');
 
-server.listen(server_port, server_ip_address, function(){
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(server_port, server_ip_address, function(){
   console.log("Listening on " + server_ip_address + ", server_port " + server_port)
 });
